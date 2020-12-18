3d18da8df15eb0ea27504230ea5e6f166bdb947d
ngan-nguyen <nganmythanh1999@gmail.com>
2020-12-18 22:10:01 +0700

    Add app bar Login view

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,4 @@
 import React, {ElementRef, FC, useEffect, useRef} from 'react';
-import mainStyle from '../../styles/mainStyle';
-import logo from '../../assets/logo.jpg';
 import Box from '@material-ui/core/Box';
 import InputLabel from '@material-ui/core/InputLabel';
 import TextInput from '../Common/TextInput';
@@ -19,7 +17,159 @@ import CustomModal from '../Common/CustomModal';
 import NetworkErrorModal from '../Common/NetworkErrorModal';
 import {useHistory} from 'react-router-dom';
 
+import { fade, makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+
+import HomeIcon from '@material-ui/icons/Home';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    grow: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      display: 'none',
+      [theme.breakpoints.up('sm')]: {
+        display: 'block',
+      },
+    },
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: fade(theme.palette.common.white, 0.15),
+      '&:hover': {
+        backgroundColor: fade(theme.palette.common.white, 0.25),
+      },
+      marginRight: theme.spacing(2),
+      marginLeft: 0,
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+      },
+    },
+    searchIcon: {
+      padding: theme.spacing(0, 2),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('md')]: {
+        width: '20ch',
+      },
+    },
+    sectionDesktop: {
+      display: 'none',
+      [theme.breakpoints.up('md')]: {
+        display: 'flex',
+      },
+    },
+    sectionMobile: {
+      display: 'flex',
+      [theme.breakpoints.up('md')]: {
+        display: 'none',
+      },
+    },
+  }),
+);
+
 const Login: FC = () => {
+	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState<null | HTMLElement>(null);
+  
+	const isMenuOpen = Boolean(anchorEl);
+	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  
+	const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+	  setAnchorEl(event.currentTarget);
+	};
+  
+	const handleMobileMenuClose = () => {
+	  setMobileMoreAnchorEl(null);
+	};
+  
+	const handleMenuClose = () => {
+	  setAnchorEl(null);
+	  handleMobileMenuClose();
+	};
+  
+	const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+	  setMobileMoreAnchorEl(event.currentTarget);
+	};
+  
+	const menuId = 'primary-search-account-menu';
+	const renderMenu = (
+	  <Menu
+		anchorEl={anchorEl}
+		anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+		id={menuId}
+		keepMounted
+		transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+		open={isMenuOpen}
+		onClose={handleMenuClose}
+	  >
+		<MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+		<MenuItem onClick={handleMenuClose}>My account</MenuItem>
+		<MenuItem onClick={handleMenuClose}>Login</MenuItem>
+		<MenuItem onClick={handleMenuClose}>Logout</MenuItem>
+	  </Menu>
+
+
+	);
+  
+	const mobileMenuId = 'primary-search-account-menu-mobile';
+	const renderMobileMenu = (
+	  <Menu
+		anchorEl={mobileMoreAnchorEl}
+		anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+		id={mobileMenuId}
+		keepMounted
+		transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+		open={isMobileMenuOpen}
+		onClose={handleMobileMenuClose}
+	  >
+
+
+		<MenuItem onClick={handleProfileMenuOpen}>
+		  <IconButton
+			aria-label="account of current user"
+			aria-controls="primary-search-account-menu"
+			aria-haspopup="true"
+			color="inherit"
+		  >
+			<AccountCircle />
+		  </IconButton>
+		  <p>Profile</p>
+		</MenuItem>
+	  </Menu>
+	);
+	
 	type TextInputHandleType = ElementRef<typeof TextInput>;
 	type CheckboxHandleType = ElementRef<typeof CustomCheckbox>;
 	type ModalHandleType = ElementRef<typeof CustomModal>;
@@ -54,7 +204,7 @@ const Login: FC = () => {
 		// dispatch action login (mo file src/saga/loginSaga.ts)
 		dispatch({type: LOGIN, dataBody, keepLogin: checkboxValue});
 	};
-
+	
 	const goToResetPassword = () => {
 		history.push('/resetPassword', {from: '/login'});
 	}
@@ -70,7 +220,47 @@ const Login: FC = () => {
 	}, [loginErrorCode]);
 
 	return (
-		<div style={{...mainStyle.fullScreen, backgroundImage: `url(${logo})`, backgroundSize: 'contain', backgroundPositionX: 'center', backgroundPositionY: 'center', backgroundRepeat: 'no-repeat', display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+		<div	>
+			<div className={classes.grow} >
+				<AppBar position="static">
+					<Toolbar>
+					<div className={classes.menuButton}>
+						<HomeIcon/>
+					</div>
+					
+					<Typography className={classes.title} variant="h6" noWrap>
+						Swaying Duck Library
+					</Typography>
+					
+					<div className={classes.search}>
+						<div className={classes.searchIcon}>
+						<SearchIcon />
+						</div>
+						<InputBase
+						placeholder="Search…"
+						classes={{
+							root: classes.inputRoot,
+							input: classes.inputInput,
+						}}
+						inputProps={{ 'aria-label': 'search' }}
+						/>
+					</div>
+					<div className={classes.grow} />
+						<MenuItem onClick={handleProfileMenuOpen}>
+							<IconButton
+							aria-label="account of current user"
+							aria-controls="primary-search-account-menu"
+							aria-haspopup="true"
+							color="inherit"
+							>
+							<AccountCircle />
+							</IconButton>					
+						</MenuItem>	
+					</Toolbar>
+				</AppBar>
+			</div>
+     		 {renderMobileMenu}
+      		{renderMenu}
 			<Box style={{width: 400, backgroundColor: '#eee', padding: 20, opacity: 0.95}} borderRadius={20} borderColor={'primary.main'} border={2}>
 				<InputLabel style={{textAlign: 'center', fontSize: 40, fontWeight: 'bold', color: 'black'}}>Login</InputLabel>
 				<InputLabel style={{color: 'black'}}>Email address</InputLabel>
